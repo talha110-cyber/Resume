568c22aa1eebddf1f65a3143a430d5bb43a1d1f5
talha110-cyber <129695603+talha110-cyber@users.noreply.github.com>
2024-09-21 19:23:41 +0500

    e

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import Homepage from './Homepage';  // Import the new Homepage component
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Homepage />} />  {/* Route for Homepage */}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
